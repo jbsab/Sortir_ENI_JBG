272bb799c3a56a2f9217237affc59274048208df
Jean Baptiste SABOURIN <136092521+jbsab@users.noreply.github.com>
2023-09-12 10:28:10 +0200

    Refont entité user et bdd

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,45 +4,118 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    #[ORM\Column(length: 180, unique: true)]
+    private ?string $username = null;
+
+    #[ORM\Column]
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
     #[ORM\Column(length: 50)]
     private ?string $lastName = null;
 
     #[ORM\Column(length: 50)]
-    private ?string $firstName = null;
+    private ?string $firstname = null;
 
-    #[ORM\Column(length: 12)]
+    #[ORM\Column(length: 12, nullable: true)]
     private ?string $phoneNumber = null;
 
-    #[ORM\Column(length: 128)]
+    #[ORM\Column(length: 180)]
     private ?string $mail = null;
-
-    #[ORM\Column(length: 50)]
-    private ?string $nickname = null;
-
-    #[ORM\Column(length: 50)]
-    private ?string $password = null;
-
-    #[ORM\Column]
-    private ?bool $isAdmin = null;
-
-    #[ORM\Column]
-    private ?bool $isActive = null;
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $profilePicture = null;
 
+    #[ORM\Column]
+    private ?bool $isActive = null;
+
+    #[ORM\Column]
+    private ?bool $isAdmin = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): static
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
+    {
+        return (string) $this->username;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): static
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): static
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials(): void
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
     }
 
     public function getLastName(): ?string
@@ -57,14 +130,14 @@ class User
         return $this;
     }
 
-    public function getFirstName(): ?string
+    public function getFirstname(): ?string
     {
-        return $this->firstName;
+        return $this->firstname;
     }
 
-    public function setFirstName(string $firstName): static
+    public function setFirstname(string $firstname): static
     {
-        $this->firstName = $firstName;
+        $this->firstname = $firstname;
 
         return $this;
     }
@@ -74,7 +147,7 @@ class User
         return $this->phoneNumber;
     }
 
-    public function setPhoneNumber(string $phoneNumber): static
+    public function setPhoneNumber(?string $phoneNumber): static
     {
         $this->phoneNumber = $phoneNumber;
 
@@ -93,38 +166,14 @@ class User
         return $this;
     }
 
-    public function getNickname(): ?string
+    public function getProfilePicture(): ?string
     {
-        return $this->nickname;
+        return $this->profilePicture;
     }
 
-    public function setNickname(string $nickname): static
+    public function setProfilePicture(?string $profilePicture): static
     {
-        $this->nickname = $nickname;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): static
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    public function isIsAdmin(): ?bool
-    {
-        return $this->isAdmin;
-    }
-
-    public function setIsAdmin(bool $isAdmin): static
-    {
-        $this->isAdmin = $isAdmin;
+        $this->profilePicture = $profilePicture;
 
         return $this;
     }
@@ -141,14 +190,14 @@ class User
         return $this;
     }
 
-    public function getProfilePicture(): ?string
+    public function isIsAdmin(): ?bool
     {
-        return $this->profilePicture;
+        return $this->isAdmin;
     }
 
-    public function setProfilePicture(?string $profilePicture): static
+    public function setIsAdmin(bool $isAdmin): static
     {
-        $this->profilePicture = $profilePicture;
+        $this->isAdmin = $isAdmin;
 
         return $this;
     }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,9 +4,6 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,27 +12,17 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nickname', TextType::class, [
-                "label"=>'Pseudo'
-            ])
-            ->add('mail', EmailType::class, [
-                "label"=>'Email'
-            ])
-            ->add('password', PasswordType::class, [
-                "label"=>'Mot de passe'
-            ])
-            ->add('lastName', TextType::class, [
-                "label"=>'Nom'
-            ])
-            ->add('firstName', TextType::class, [
-                "label"=>'Prenom'
-            ])
-            ->add('phoneNumber', TextType::class, [
-                "label"=>'Telephone'
-            ])
-            ->add('isAdmin')
+            ->add('username')
+            ->add('roles')
+            ->add('password')
+            ->add('lastName')
+            ->add('firstname')
+            ->add('phoneNumber')
+            ->add('mail')
+            ->add('profilePicture')
             ->add('isActive')
-       ;
+            ->add('isAdmin')
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
