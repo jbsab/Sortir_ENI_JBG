d7bb49787675fceb106efc2551426e2e8ff63cf5
Jean Baptiste SABOURIN <136092521+jbsab@users.noreply.github.com>
2023-09-12 14:07:18 +0200

    Creation LoginForm

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +13,25 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('roles')
-            ->add('password')
-            ->add('lastName')
-            ->add('firstname')
-            ->add('phoneNumber')
+            ->add('username', TextType::class,[
+                "label" => 'Pseudo'
+            ])
+            ->add('password', TextType::class,[
+                "label" => 'Mot de passe'
+            ])
+            ->add('lastName', TextType::class,[
+                "label" => 'Nom'
+            ])
+            ->add('firstname', TextType::class, [
+                "label" => 'Prenom'
+            ])
+            ->add('phoneNumber', TextType::class,[
+                "label" => 'Numero de telephone'
+            ])
             ->add('mail')
-            ->add('profilePicture')
+            ->add('profilePicture', TextType::class, [
+                "label" => 'Photo de profil'
+            ])
             ->add('isActive')
             ->add('isAdmin')
         ;
